f51b5b51abafa586100ea26fd2f04a6797f52178
13033266796 <929315627@qq.com>
2019-10-16 21:32:42 +0800

    first

---
+++ homeWorkShut.py
@@ -0,0 +1,31 @@
+# -*-coding:utf-8-*-
+
+class Person(object):
+    #拿枪
+    #开枪
+    pass
+
+class Gun(object):
+    #装弹夹
+    #开枪
+
+    pass
+
+#弹夹类
+class Clip(object):
+    #装子弹
+    #开枪消耗子弹
+    pass
+
+#子弹类
+class Bullet(object):
+    pass
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
